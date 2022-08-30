6e3426297d3e1e6746ea5b7e0bfa4fc105a0a970
Tarek <83262890+tarek102@users.noreply.github.com>
2022-08-30 10:56:01 +0200

    setup React-Redux

---
+++ src/redux/ConfigureStore.js
@@ -0,0 +1,15 @@
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import thunk from "redux-thunk";
+
+
+const rootReducer = combineReducers ({
+  missions: missionReducer,
+  rockets: rocketReducer,
+});
+
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: [thunk],
+});
+
+export default store;
