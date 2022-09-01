161d709e0691e2de7789dc6b1f7df0c949b0bf12
Tarek <83262890+tarek102@users.noreply.github.com>
2022-09-01 11:35:44 +0200

    style rockets

--- src/components/Rocket.js
+++ src/components/Rocket.js
@@ -3,6 +3,7 @@
 import React from 'react';
 import { useDispatch } from 'react-redux/es/exports';
 import { PropTypes } from 'prop-types';
+import Badge from 'react-bootstrap/Badge';
 import { rocketBook, rocketCancel } from '../redux/rockets/rockets';
 
 const Rocket = (props) => {
@@ -17,18 +18,22 @@ const Rocket = (props) => {
   } = props;
 
   return (
-    <div id={id} key={id} className="d-flex flex-wrap flex-lg-nowrap m-2 p-2">
-      <img src={flickr_images} width="250" height="180" className="image mx-2" alt="rocket-one" />
-      <div className="wrapper">
+    <div id={id} key={id} className="d-flex flex-wrap flex-lg-nowrap my-2 py-2">
+      <img src={flickr_images} width="290" height="220" className="image mx-2" alt="rocket-one" />
+      <div className="wrapper d-flex flex-column justify-content-start h-5">
         <h2 className="rocketName">{rocket_name}</h2>
-        <div className="description">
-          {reserved && <h4>Reserved</h4>}
+        <div className="description my-3">
+          {reserved && (
+          <h5>
+            <Badge bg="secondary">reserved</Badge>
+          </h5>
+          )}
           {description}
         </div>
         {reserved ? (
           <button
             id={id}
-            className="cancelBtn btn btn-outline-danger"
+            className="cancelBtn btn btn-outline-danger w-25"
             type="button"
             onClick={() => dispatch(rocketCancel(id))}
           >
@@ -37,7 +42,7 @@ const Rocket = (props) => {
         ) : (
           <button
             id={id}
-            className="reserveBtn btn btn-outline-info"
+            className="reserveBtn btn btn-outline-info w-25"
             type="button"
             onClick={() => dispatch(rocketBook(id))}
           >
